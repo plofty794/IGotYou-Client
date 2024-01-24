0b24467ad9a4aea62164632bb4029ac0ba811016
Ace G <aceguevarra48@gmail.com>
2024-01-24 09:27:32 +0800

    Fixed

--- src/api/axiosRoute.ts
+++ src/api/axiosRoute.ts
@@ -1,11 +1,7 @@
 import axios from "axios";
 
-export const axiosRoute = axios.create({
-  baseURL: "https://i-got-you-api.vercel.app",
-});
-
 export const axiosPrivateRoute = axios.create({
-  baseURL: "https://i-got-you-api.vercel.app",
+  baseURL: "https://i-got-you-api.vercel.app/",
   withCredentials: true,
   headers: { Authorization: `Bearer ${localStorage.getItem("token")}` },
 });
