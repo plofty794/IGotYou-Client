a9fe3b39463b587600b1c740f03d611e0b568320
Ace G <aceguevarra48@gmail.com>
2024-03-19 11:04:03 +0800

    Updated code

--- src/api/axiosRoute.ts
+++ src/api/axiosRoute.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosPrivateRoute = axios.create({
-  baseURL: "http://localhost:5040",
+  baseURL: "https://igot-you.online/api",
   withCredentials: true,
   headers: {
     Authorization: `Bearer ${localStorage.getItem("token")}`,
